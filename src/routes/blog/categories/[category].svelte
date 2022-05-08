b7664df5fe71d3e68ee724f9844a9c71d5cc744f
Levente Krizsan <lewalevi@gmail.com>
2022-05-08 13:24:53 +0200

    feat(api): use page endpoint for categories section

--- src/routes/blog/categories/[category].svelte
+++ src/routes/blog/categories/[category].svelte
@@ -1,20 +1,3 @@
-<script context="module">
-    export async function load({ params, fetch }) {
-        const currentCategory = params.category;
-        const response = await fetch('/api/posts.json');
-        const posts = await response.json();
-
-        const matchingPosts = posts.filter(post => post.meta.categories.includes(currentCategory));
-
-        return {
-            props: {
-                category: currentCategory,
-                posts: matchingPosts,
-            },
-        };
-    }
-</script>
-
 <script>
     import PostList from '$lib/components/post-list.svelte';
 
