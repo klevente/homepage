dd237dc8416c79ae1e564c03546c63ca4b16b746
Levente Krizsán <lewalevi@gmail.com>
2023-01-21 15:16:49 +0100

    feat(project): add scss

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,4 @@
+<script>
+</script>
+
+<header>Header</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import '$lib/styles/style.scss';
+  import Header from '$lib/components/header.svelte';
+</script>
+
+<Header />
+
+<main>
+  <slot />
+</main>
+
+<footer>Footer</footer>
