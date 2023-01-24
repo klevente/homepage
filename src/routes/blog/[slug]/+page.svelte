dcdcd5fdba1d78d291f14295851d506c6890d400
Levente Krizsán <lewalevi@gmail.com>
2023-01-24 22:37:07 +0100

    feat(blog): add title to post

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import type { PageData } from './$types';
+  import { formatTitle } from '$lib/utils/format-title.js';
 
   export let data: PageData;
   const { title, date, content } = data;
 </script>
+
+<svelte:head>
+  <title>{formatTitle(title)}</title>
+</svelte:head>
 
 <article>
   <h1>{title}</h1>
