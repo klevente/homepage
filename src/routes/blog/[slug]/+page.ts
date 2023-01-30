b76dc34bee7f7b8b4b0a46cb40cbc3974e6257e5
Levente Krizsán <lewalevi@gmail.com>
2023-01-30 19:00:57 +0100

    fix(blog-post): use correct path for metadata import

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,6 +1,7 @@
 import type { PageLoad } from './$types';
 import type { SvelteComponentTyped } from 'svelte';
-import type { Empty, Metadata } from '$lib/types';
+import type { Empty } from '$lib/types';
+import type { Metadata } from '$lib/utils/fetch-posts';
 
 export const load = (async ({ params }) => {
   const post = (await import(`../${params.slug}.md`)) as {
