0f8f4d87ae877d046eb480817f51887917eef59d
Levente Krizsán <lewalevi@gmail.com>
2023-01-30 19:02:06 +0100

    chore(project): remove unused code

--- src/lib/utils/format-file-size.ts
+++
@@ -1,8 +0,0 @@
-import prettyBytes from 'pretty-bytes';
-
-export function formatFileSize(size: number, withSpace: boolean = false): string {
-  return prettyBytes(size, {
-    spaces: withSpace ? 1 : 0,
-    uppercaseKilo: true,
-  });
-}
