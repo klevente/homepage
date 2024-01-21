aac049fbea6da8687e6ed2fa922b78f89dba8dee
Levente Krizsan <lewalevi@gmail.com>
2024-01-21 11:49:37 +0100

    fix(blog): show 404 page for nonexistent blog post when not prerendering

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,21 +1,26 @@
 import type { PageLoad } from "./$types";
 import type { ComponentType } from "svelte";
 import type { Metadata } from "$lib/utils/fetch-posts";
+import { error } from "@sveltejs/kit";
 
 export const load = (async ({ params }) => {
-  const post = (await import(`../../../../content/posts/${params.slug}.md`)) as {
-    metadata: Metadata;
-    default: ComponentType;
-  };
+  try {
+    const post = (await import(`../../../../content/posts/${params.slug}.md`)) as {
+      metadata: Metadata;
+      default: ComponentType;
+    };
 
-  const {
-    metadata: { title, date },
-    default: content,
-  } = post;
+    const {
+      metadata: { title, date },
+      default: content,
+    } = post;
 
-  return {
-    content,
-    title,
-    date,
-  };
+    return {
+      content,
+      title,
+      date,
+    };
+  } catch (e) {
+    error(404);
+  }
 }) satisfies PageLoad;
